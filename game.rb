1c69d7ad44338904759faad60436e9bb74aaf19b
ste001 <asso99@hotmail.com>
2018-11-27 17:48:50 +0100

    Add save and load functions

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 def create_dictionary file
   dictionary = []
   File.open(file).each do |word|
@@ -36,26 +38,83 @@ def game_over? (solution, guess)
   solution == guess
 end
 
+def save_game (solution, guess, tries, wrong_letters)
+  save = YAML.dump({
+    :solution => solution,
+    :guess => guess,
+    :tries => tries,
+    :wrong_letters => wrong_letters
+  })
+  File.write("save.yaml", save)
+  puts "Game saved!"
+end
+
+def load_game (file, solution, guess, tries, wrong_letters)
+  if (File.exist? file)
+    data = YAML.load File.read(file)
+    solution = data[:solution]
+    guess = data[:guess]
+    tries = data[:tries]
+    wrong_letters = data[:wrong_letters]
+    puts "Game loaded!"
+    [solution, guess, tries, wrong_letters]
+  else
+    puts "WARNING: No saved data"
+    false
+  end
+end
+
 dictionary = create_dictionary "5desk.txt"
-solution = random_word dictionary
+solution = (random_word dictionary).downcase
 guess = create_guess_stub solution
 tries = 0
 wrong_letters = []
-puts guess
+
+puts "Do you want to start a new game or load a saved game?"
+puts "Press 1 to load a game, any other key to start a new one."
+choice = gets.chomp.to_i
+case choice
+when 1
+  loaded = load_game("save.yaml", solution, guess, tries, wrong_letters)
+  if (loaded)
+    solution = loaded[0]
+    guess = loaded[1]
+    tries = loaded[2]
+    wrong_letters = loaded[3]
+    puts guess
+    puts wrong_letters.join(", ")
+  else
+    puts "New game starting..."
+    puts guess
+  end
+else
+  puts "New game starting..."
+  puts guess
+end
 
 while (not game_over?(solution, guess) || tries >= 6)
-  puts "Insert a letter: "
-  letter = gets.chomp.downcase!
-  if (solution.include? letter)
-    insert_letters(letter, solution, guess)
+  puts "Do you want to insert a letter or save the game?"
+  puts "Press 1 to insert a letter, 2 to save the game"
+  choice = gets.chomp.to_i
+  case choice
+  when 1
+    puts "Insert a letter: "
+    letter = gets.chomp.downcase
+    if (solution.include? letter)
+      insert_letters(letter, solution, guess)
+    else
+      wrong_letters.push(letter)
+      tries += 1
+    end
+  when 2
+    save_game(solution, guess, tries, wrong_letters)
   else
-    wrong_letters.push(letter)
-    tries += 1
+    "No inputs defined for this key!"
   end
   puts guess
   puts wrong_letters.join(", ")
 end
 
-tries >= 6 ? "You lost!" : "You win!"
+puts tries >= 6 ? "You lost!" : "You win!"
 
   
